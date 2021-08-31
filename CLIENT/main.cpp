241f0064c020402dd84869342ba557fba145a3c0
Kat9_123 <56130489+Kat9-123@users.noreply.github.com>
2021-08-31 10:43:46 +0200

    Update main.cpp

--- CLIENT/main.cpp
+++ CLIENT/main.cpp
@@ -28,7 +28,7 @@ using namespace std;
 // EDIT THESE VALUES
 const string POST_FILE  = ; // Path for SERVER - CLIENT communication
 const string GET_FILE = ;  // Path for CLIENT - SERVER communication
-const string GET_CACHE =  // Path for cache
+const string GET_CACHE =  ;// Path for cache
 
 // AES key
 unsigned char key[] = { 0x00, 0x01, 0x02, 0x03, 0x04, 0x05, 0x06, 0x07, 0x08, 0x09, 0x0a, 0x0b, 0x0c, 0x0d, 0x0e, 0x0f, 0x10, 0x11,
